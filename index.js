0502124b8ca15b36dd24672fe354e4caf7c10a27
sanuarKhan <sanuarkhan220@gmail.com>
2025-02-23 02:32:00 +0600

    reading file issues solved in index.js

--- index.js
+++ index.js
@@ -3,51 +3,45 @@ const fs = require('fs');
 const url = require('url');
 
 const server = http.createServer((req, res) => {
-if (req.url === '/') {
-    fs.readFile('index.html', (err, data) => {
+    let filePath;
+
+    // Determine the file path based on the request URL
+    if (req.url === '/') {
+        filePath = 'index.html';
+    } else if (req.url === '/about') {
+        filePath = 'about.html';
+    } else if (req.url === '/contact-me') {
+        filePath = 'contact-me.html';
+    } else {
+        filePath = '404.html';
+    }
+
+    // Read the file and send the response
+    fs.readFile(filePath, (err, data) => {
         if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end('404 Not Found');
+            // If the file is not found, send a 404 response
+            if (filePath === '404.html') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                return res.end('404 Not Found');
+            } else {
+                // If any other error occurs, redirect to 404.html
+                fs.readFile('404.html', (err, data) => {
+                    if (err) {
+                        res.writeHead(404, { 'Content-Type': 'text/html' });
+                        return res.end('404 Not Found');
+                    }
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.write(data);
+                    return res.end();
+                });
+            }
+        } else {
+            // If the file is found, send it with a 200 status code
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end();
         }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    })
-    }
-    if (req.url === '/about') {
-        fs.readFile('about.html', (err, data) => {
-            if (err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end('404 Not Found');
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            return res.end();
-        })
-    }
-    if (req.url === '/contact-me') {
-        fs.readFile('contact-me.html', (err, data) => {
-            if (err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end('404 Not Found');
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            return res.end();
-        })
-    }
-    else {
-       fs.readFile('404.html', (err, data) => {
-           if (err) {
-               res.writeHead(404, {'Content-Type': 'text/html'});
-               return res.end('404 Not Found');
-           }
-           res.writeHead(200, {'Content-Type': 'text/html'});
-           res.write(data);
-           return res.end();
-       })   
-    }
-
-
-}).listen(8080, () => { console.log('Server is running...') }
-);
+    });
+}).listen(8080, () => {
+    console.log('Server is running on port 8080...');
+});
